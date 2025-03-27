dc672dcb6576e9348c36a2714ed34e618087842d
almeidayuri <159081218+almeidayuri@users.noreply.github.com>
2025-03-27 20:15:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -72,3 +72,9 @@ window.addEventListener("beforeinstallprompt", (event) => {
         }
     });
 });
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js")
+    .then(() => console.log("Service Worker registrado!"))
+    .catch((error) => console.error("Erro ao registrar o Service Worker:", error));
+}
